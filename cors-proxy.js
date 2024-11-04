5405e579214fa5416afd6641f2466a7e9f17a74f
kalair22 <kalaivaniravi2208@gmail.com>
2024-11-04 19:07:26 +0530

    api

---
+++ cors-proxy.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+
+app.get("/proxy", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.hercules.exchange/v2/tokens");
+    res.json(response.data);
+  } catch (error) {
+    res
+      .status(error.response ? error.response.status : 500)
+      .send("Error fetching data");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`CORS Proxy running on http://localhost:${PORT}`);
+});
